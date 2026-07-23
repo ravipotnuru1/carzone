94d97a1df382151566ff78ca4e7270114d7df61e
ravipotnuru1 <potnururavi@thestackly.com>
2026-07-23 10:48:38 +0530

    Completed car project

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -2,14 +2,23 @@ import Navbar from "../components/layout/Navbar";
 import ServiceDetails from "../components/services/ServiceDetails";
 import Footer from "../components/home/Footer";
 
+import shippingImg from "../assets/images/services/service-shipping.png";
+
 const Shipping = () => {
   return (
     <>
       <Navbar />
 
-      <main>
-        <ServiceDetails />
-      </main>
+      <ServiceDetails
+        title="SHIPPING"
+        image={shippingImg}
+        buttonText="Deliver my car"
+        paragraphs={[
+          "Our worldwide shipping service ensures your classic vehicle reaches you safely and securely, no matter where you are.",
+          "We work with trusted logistics partners who specialize in transporting valuable automobiles using enclosed carriers and professional handling.",
+          "From pickup to final delivery, we keep you informed throughout the process so your vehicle arrives in excellent condition."
+        ]}
+      />
 
       <Footer />
     </>
